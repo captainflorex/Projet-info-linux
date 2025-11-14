b0658c01ffce03321c62c5809832c8e3fb0568c9
remimtd <remi.matteoda@edu.ece.fr>
2025-11-14 13:58:37 +0100

    Update sauvegarde.h

--- sauvegarde.h
+++ sauvegarde.h
@@ -1,19 +1,11 @@
-/*
-* ════════════════════════════════════════════════════════════════════════════
- * FICHIER: sauvegarde.h
- * Description: Module de sauvegarde - Header
- * ════════════════════════════════════════════════════════════════════════════
- */
-
 #ifndef SAUVEGARDE_H
 #define SAUVEGARDE_H
-
-#include "structures.h"
 
 #define FICHIER_SAUVEGARDE "sauvegardes.txt"
 
 // Fonctions de sauvegarde
 int sauvegarderPartie(char* pseudo, int niveau, int vies);
+
 int chargerPartie(char* pseudo, int* niveau, int* vies);
 
 #endif // SAUVEGARDE_H
