be611f97845ab87ef736d300ef97f8fa9b380306
remimtd <remi.matteoda@edu.ece.fr>
2025-12-17 11:13:07 +0100

    Update sauvegarde.h

--- sauvegarde.h
+++ sauvegarde.h
@@ -1,17 +1,22 @@
 #ifndef SAUVEGARDE_H
 #define SAUVEGARDE_H
+#include "structures.h"
 
 #define FICHIER_SAUVEGARDE "sauvegardes.txt"
 
-// Fonctions de sauvegarde
+// Sauvegarde simple (Progression générale)
 int sauvegarderPartie(char* pseudo, int niveau, int vies);
-
 int chargerPartie(char* pseudo, int* niveau, int* vies);
+
+// NOUVEAU : Sauvegarde Complète (État exact du jeu)
+int sauvegarderPartieComplete(char* pseudo, Partie* partie);
+int chargerPartieComplete(char* pseudo, Partie* partie);
 
 typedef struct {
     char pseudo[50];
     int niveau;
     int vies;
 } Enregistrement;
+
 
 #endif // SAUVEGARDE_H
